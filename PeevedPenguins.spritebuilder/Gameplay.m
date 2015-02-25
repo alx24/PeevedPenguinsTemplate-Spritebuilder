b109d08802bef4923de5dbe127070dd4f5e07e64
alex ferreira <abrex18@hotmail.com>
2015-02-24 23:19:05 -0300

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -13,10 +13,34 @@
     CCPhysicsNode* _physicsNode;
     CCNode* _catapultArm;
     
+}
+
+- (void)didLoadFromCCB
+{
     
+    self.userInteractionEnabled = YES;
+}
+
+-(void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+    [self luachPenguin];
     
 }
 
+// metado para lanca peguin da cataputa
+-(void)luachPenguin
+{
+    // carregando peguin definido no spritebuilde
+    CCNode* penguin = [CCBReader load:@"Pinguim"];
+    
+    penguin.position = ccpAdd(_catapultArm.position, ccp(16,50));
+    
+    [_physicsNode addChild:penguin];
+    
+    CGPoint launchDirection = ccp(1, 0);
+    CGPoint force = ccpMult(launchDirection, 8000);
+    [penguin.physicsBody applyForce:force];
+}
 
 
 
