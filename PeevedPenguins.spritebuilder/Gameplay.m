eb04da0664c8fc4bf75db43965d2cfe7a9db9abb
alex ferreira <abrex18@hotmail.com>
2015-02-24 23:01:43 -0300

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -0,0 +1,27 @@
+//
+//  Gameplay.m
+//  PeevedPenguins
+//
+//  Created by Macbook White on 24/02/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay
+{
+    CCPhysicsNode* _physicsNode;
+    CCNode* _catapultArm;
+    
+    
+    
+}
+
+-(void)teste{
+    
+    NSLog(@"vairavel phiscs e ctapult amr criadas");
+}
+
+
+
+@end
