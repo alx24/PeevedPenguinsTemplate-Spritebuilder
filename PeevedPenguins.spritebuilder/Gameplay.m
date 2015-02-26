f992b9a326d0b682b6f96de9060ffeca258af13c
alex ferreira <abrex18@hotmail.com>
2015-02-26 01:03:53 -0300

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -11,10 +11,12 @@
 @implementation Gameplay
 {
     CCPhysicsNode* _physicsNode;
+    CCPhysicsJoint *_mouseJoint;
     CCNode* _catapultArm;
     CCNode* _levelNode;
     CCNode* _nodeScrol;
     CCNode* _pullbackNode;
+    CCNode* _mouseJointNode;
     
 }
 
@@ -28,32 +30,78 @@
     
     _physicsNode.debugDraw = YES;
     _pullbackNode.physicsBody.collisionMask = @[];
+    _mouseJointNode.physicsBody.collisionMask = @[];
 }
 
 -(void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event
 {
-    [self luachPenguin];
+    CGPoint touchLocation = [touch locationInNode:_nodeScrol];
+    
+    // start catapult dragging when a touch inside of the catapult arm occurs
+    if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation))
+    {
+        // move the mouseJointNode to the touch position
+        _mouseJointNode.position = touchLocation;
+        
+        // setup a spring joint between the mouseJointNode and the catapultArm
+        _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:
+                       _mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0)
+                        anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
+    
+   }
+
+}
+
+-(void)touchMoved:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+    // whenever touches move, update the position of the mouseJointNode to the touch position
+    CGPoint touchLocation = [touch locationInNode: _nodeScrol];
+    _mouseJointNode.position = touchLocation;
+}
+
+
+-(void)releaseCatapult
+{
+    if (_mouseJoint != nil);
+    {
+        [_mouseJoint invalidate];
+        _mouseJoint = nil;
+    }
+    
     
 }
 
-// metado para lanca peguin da cataputa
--(void)luachPenguin
+
+-(void) touchEnded:(UITouch *)touch withEvent:(UIEvent *)event
 {
-    // carregando peguin definido no spritebuilde
-    CCNode* penguin = [CCBReader load:@"Penguins"];
-    
-    penguin.position = ccpAdd(_catapultArm.position, ccp(16,50));
-    
-    [_physicsNode addChild:penguin];
-    
-    CGPoint launchDirection = ccp(1, 0);
-    CGPoint force = ccpMult(launchDirection, 8000);
-    [penguin.physicsBody applyForce:force];
-    self.position = ccp(0,0);
-    CCActionFollow *folow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-    [_nodeScrol runAction:folow];
-    
+    // when touches end, meaning the user releases their finger, release the catapult
+    [self releaseCatapult];
 }
+
+-(void) touchCancelled:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    // when touches are cancelled, meaning the user drags their finger off the screen or onto something else, release the catapult
+    [self releaseCatapult];
+}
+
+// metado para lanca peguin da cataputa
+//-(void)luachPenguin
+//{
+//    // carregando peguin definido no spritebuilde
+//    CCNode* penguin = [CCBReader load:@"Penguins"];
+//    
+//    penguin.position = ccpAdd(_catapultArm.position, ccp(16,50));
+//    
+//    [_physicsNode addChild:penguin];
+//    
+//    CGPoint launchDirection = ccp(1, 0);
+//    CGPoint force = ccpMult(launchDirection, 8000);
+//    [penguin.physicsBody applyForce:force];
+//    self.position = ccp(0,0);
+//    CCActionFollow *folow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
+//    [_nodeScrol runAction:folow];
+//    
+//}
 
 -(void)retry
 {
